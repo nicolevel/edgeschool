8a1a6ebebf99a87df059e9ac99fe01946fa8a534
Nicole Velasco <nicolevelasco455@gmail.com>
2019-02-14 13:33:04 -0400

    added pages view

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,5 @@ Rails.application.routes.draw do
   resources :pages
   resources :gallery
 
-  get 'pages/contact'  => "pages#contact"
-  get 'pages/whatweoffer' => "pages#whatweoffer"
-  get 'pages/whoweare' => "pages#whoweare"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
