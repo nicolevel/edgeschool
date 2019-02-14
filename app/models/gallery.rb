10771e84e39fdab5c3419179caf69071fb6b3464
Nicole Velasco <nicolevelasco455@gmail.com>
2019-02-14 13:04:52 -0400

    added admin, photo, blog and gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,2 +1,3 @@
 class Gallery < ApplicationRecord
+   mount_uploader :photo, PhotoUploader
 end
---
+++ db/migrate/20190214164556_add_photo_to_gallery.rb
@@ -0,0 +1,5 @@
+class AddPhotoToGallery < ActiveRecord::Migration[5.2]
+  def change
+    add_column :galleries, :photo, :string
+  end
+end
