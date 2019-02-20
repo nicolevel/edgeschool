a4ee53e385040c4121adecd54cc0bacf577d4d4b
Nicole Velasco <nicolevelasco455@gmail.com>
2019-02-20 14:11:15 -0400

    cover pic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :blogs
   devise_for :users
   root to: 'pages#home'
-  resources :pages
+  get 'pages/contact', as: 'contact'
+  get 'pages/whatweoffer', as: 'whatweoffer'
+  get 'pages/whoweare', as: 'whoweare'
   resources :gallery
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
