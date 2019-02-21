4047fcafc13f7b90f9c800f1d024d19a6a4f020d
Nicole Velasco <nicolevelasco455@gmail.com>
2019-02-21 18:14:49 -0400

    contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,26 @@
 class MessagesController < ApplicationController
 def contact
-    @contact = Message.new(params[:message])
-  end
+  @contact = Message.new(params[:message])
+end
 
-  def create
-    @contact = Message.new(params[:message])
-    @contact.request = request
-    respond_to do |format|
-      if @contact.deliver
-        # re-initialize Home object for cleared form
-        @contact = Home.new
-        format.html { render 'contact'}
-        format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
-      else
-        format.html { render 'contact' }
-        format.js   { flash.now[:error] = @message = "Message did not send." }
-      end
+def index
+end
+
+def create
+  @contact = Message.new(params[:message])
+  @contact.request = request
+  respond_to do |format|
+    if @contact.deliver
+      # re-initialize Home object for cleared form
+      @contact = Message.new
+      format.html { render 'index', :layout => false }
+      format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
+    else
+      format.html { render 'shared/contact', :layout => false  }
+      format.js   { flash.now[:error] = @message = "Message did not send." }
+    end
     end
   end
+
+
 end
