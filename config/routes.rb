51136d2f157b4adc93f4dede086fcc3c967191bf
Kayra Czerniewicz <kayracz@gmail.com>
2019-02-21 14:06:32 -0400

    index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :blogs
+  resources :gallery
   devise_for :users
-  root to: 'pages#home'
+
   get 'pages/contact', as: 'contact'
   get 'pages/whatweoffer', as: 'whatweoffer'
   get 'pages/whoweare', as: 'whoweare'
-  resources :gallery
-  get 'messages#new'
-  post 'messages#create'
+  # get 'messages#new'
+  # post 'messages#create'
 
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
