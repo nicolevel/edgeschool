80fc9f4a8ee15a92c0fe0b5820328aff84c246cb
Nicole Velasco <nicolevelasco455@gmail.com>
2019-03-07 21:10:06 -0400

    contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,5 @@
 class MessagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:contact, :index, :create]
-
-def contact
-  @contact = Message.new(params[:message])
-end
+  skip_before_action :authenticate_user!, only: [:index, :create]
 
 def index
   @contact = Message.new(params[:message])
@@ -12,12 +8,11 @@ end
 def create
   @contact = Message.new(params[:message])
   @contact.request = request
-  @contact = Message.new(params[:message])
   if @contact.valid?
-      flash[:notice] = "Message sent! Thank you for contacting us. We'll get back to you soon."
-      redirect_to root_url
+    flash[:notice] = "Message sent! Thank you for contacting us. We'll get back to you soon."
+    redirect_to root_url
   else
-      render :action => 'new'
-    end
+    render :action => 'new'
   end
+end
 end
