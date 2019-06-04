94bef435ae06010e09a294cd485f263de7c91283
Kayra Czerniewicz <kayracz@gmail.com>
2019-06-04 12:31:47 -0400

    access to admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,7 +1,7 @@
 RailsAdmin.config do |config|
   ### Popular gems integration
-  config.authorize_with do |controller|
-    redirect_to main_app.root_path unless current_user
+  # config.authorize_with do |controller|
+  #   redirect_to main_app.root_path unless current_user
   end
 
   # # [...]
